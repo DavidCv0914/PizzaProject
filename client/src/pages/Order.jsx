0bf40836b2ea41c26e41ad085d955878c7f9b4ea
DavidCv0914 <dabeats2004@gmail.com>
2023-05-12 11:41:49 -0500

    gggg

--- client/src/pages/Order.jsx
+++ client/src/pages/Order.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 
 export const Order = () => {
+
+    const [isChecked, setIsChecked] = useState(false);
+
+    function handleCheckboxChange() {
+      setIsChecked(!isChecked);
+    }
+    console.log(isChecked);
+
     return(
+        <>
         <h1>hellow world</h1>
+        <form>
+        <input 
+          type="checkbox" 
+          checked={isChecked} 
+          onChange={handleCheckboxChange} 
+        />
+        </form>
+        </>
     )
 }

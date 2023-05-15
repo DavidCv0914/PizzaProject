b7da566ca50825e68ba1e5fe7df587d9ab911da3
DavidCv0914 <dabeats2004@gmail.com>
2023-05-15 11:43:49 -0500

    hhhh

--- client/src/pages/Order.jsx
+++ client/src/pages/Order.jsx
@@ -1,24 +1,231 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 export const Order = () => {
+  const [isChecked, setIsChecked] = useState(false);
 
-    const [isChecked, setIsChecked] = useState(false);
+  function handleCheckboxChange() {
+    setIsChecked(!isChecked);
+  }
 
-    function handleCheckboxChange() {
-      setIsChecked(!isChecked);
-    }
-    console.log(isChecked);
-
-    return(
-        <>
-        <h1>hellow world</h1>
-        <form>
-        <input 
-          type="checkbox" 
-          checked={isChecked} 
-          onChange={handleCheckboxChange} 
+  return (
+    <>
+      <h1>hellow world</h1>
+      <form>
+        <h3>Carnes</h3>{/*2*/}
+        <p>Pepperoni</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
         />
-        </form>
-        </>
-    )
-}
+        <p>Jamón</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Salchicha</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Tocino</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Pollo</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Carne Molida</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Salami</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Chorizo</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Carne Molida</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Prosciuto</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+      </form>
+
+      <form>
+        <h3>Queso</h3>{/*1*/}
+        {/*2*/}
+        <p>Mozarella</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Parmesano</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Cheddar</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Feta</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Cabra</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Ricotta</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Gorgonzola</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Azul</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+      </form>
+
+
+
+      
+      <form>
+      <h3>Vegetales</h3>{/*3*/}
+        <p>Champiñones</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Pimiento verde</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Maiz</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Cebolla</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Tomate</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Espinacas</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Piña</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Tomates secos</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+      </form>
+
+
+
+      
+      <form>
+      <h3>Salsas</h3>{/*2*/}
+        <p>Salsa de tomate</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Salsa BBQ</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Salsa Alfredo</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Salsa pesto</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Salsa Ranchera</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <p>Salsa de ajo</p>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+      </form>
+      <h3>Aderezos</h3>
+      {/*3*/}
+    </>
+  );
+};

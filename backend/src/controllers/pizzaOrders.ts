3be060b2f72fa7a7cd850f4e2d1810328c506ff8
Jackson Londoño <alexisjacksonlon213@gmail.com>
2023-05-24 10:13:41 -0500

    backend organizado

--- backend/src/controllers/pizzaOrders.ts
+++ backend/src/controllers/pizzaOrders.ts
@@ -12,7 +12,7 @@ class PizzaOrder {
     public async generateOrder(req:Request,res:Response) {
         try {
             const token = req.header("token")
-            const {ingredient1,ingredient2,ingredient3,ingredient4,ingredient5,size,cost,address} = req.body
+            const {ingredient1,ingredient2,ingredient3,ingredient4,ingredient5,size,cost,address} = req.body || "no hay"
             const ingredient6 = req.body.ingredient6 || "no hay"
             const ingredient7 = req.body.ingredient7 || "no hay"
             const ingredient8 = req.body.ingredient8 || "no hay"
@@ -24,25 +24,39 @@ class PizzaOrder {
                 let {email}:any = emailVerify;
                 
                 const response:any[]=await conexion.query("INSERT INTO ingredientes(ingrediente1, ingrediente2, ingrediente3, ingrediente4, ingrediente5, ingrediente6, ingrediente7, ingrediente8, ingrediente9) VALUES (?,?,?,?,?,?,?,?,?)",[ingredient1,ingredient2,ingredient3,ingredient4,ingredient5,ingredient6,ingredient7,ingredient8,ingredient9]);
-
+                const responseIngredients: any[] = await conexion.query("SELECT * FROM ingredientes WHERE id_lista = LAST_INSERT_ID()");
                 console.log("-----------------------------------");
-                if (response[0].affectedRows != 0) {
-                    const responsePizza:any[] = await conexion.query("INSERT INTO pizza(tamaño,precio) VALUES (?,?)",[size,cost]);
-                    if (responsePizza[0].affectedRows !=0) {
-                        const responseIngredients: any[] = await conexion.query("SELECT * FROM ingredientes WHERE id_lista = LAST_INSERT_ID()");
-                        const responseRecipePizza: any[] = await conexion.query("SELECT * FROM pizza WHERE id_pizza = LAST_INSERT_ID()");
 
-                        if (responseIngredients[0].length > 0 && responseRecipePizza[0].length > 0) {
-                            const insertRecipe:any[] = await conexion.query("INSERT INTO receta(id_pizza2,id_lista1) VALUES (?,?)",[responseRecipePizza[0][0].id_pizza,responseIngredients[0][0].id_lista]);
-                            if (insertRecipe[0].affectedRows != 0) {
-                                const order : any[] = await conexion.query("INSERT INTO pedido(id_pizza1,email_profile,direccion) VALUES (?,?,?)",[responseRecipePizza[0][0].id_pizza,email,address]);
-                                if (order[0].affectedRows != 0) {
-                                    res.json("Generate order");
+                if (response[0].affectedRows != 0) {
+
+                    const pedido : any[] = await conexion.query("INSERT INTO pedido(email_profile,direccion) VALUES (?,?)",[email,address]);
+
+                    if (pedido[0].affectedRows != 0) {
+                        
+                        const responsePedido : any[] = await conexion.query("SELECT * FROM pedido WHERE id_pedido = LAST_INSERT_ID()");
+                        console.log(responseIngredients[0]);
+                        
+                        if (responseIngredients[0].length > 0 && responsePedido[0].length > 0) {
+                            const responsePizza:any[] = await conexion.query("INSERT INTO pizza(tamaño,precio,id_pedido1) VALUES (?,?,?)",[size,cost,responsePedido[0][0].id_pedido]);
+                            if (responsePizza[0].affectedRows !=0) {
+                                const responseRecipePizza: any[] = await conexion.query("SELECT * FROM pizza WHERE id_pizza = LAST_INSERT_ID()");
+                                const insertRecipe:any[] = await conexion.query("INSERT INTO receta(id_pizza1,id_lista1) VALUES (?,?)",[responseRecipePizza[0][0].id_pizza,responseIngredients[0][0].id_lista]);
+                                if (insertRecipe[0].affectedRows != 0) {
+                                    res.json("generate order")
+                                }else{
+                                    res.json("entonces")
                                 }
+                            }else{
+                                res.json("no generate")
                             }
-                            
-                        } 
-                    } 
+                        }else{
+                            res.json("no generate")
+                        }
+                    }else{
+                        res.json("no generate")
+                    }
+                }else{
+                    res.json("no generate")
                 }
             }else{
                 res.json("no loged")
--- client/src/pages/Order.jsx
+++ client/src/pages/Order.jsx
@@ -13,6 +13,24 @@ export const Order = () => {
   const [isChecked9, setIsChecked9] = useState(false);
   const [isChecked10, setIsChecked10] = useState(false);
   const [validateForm1, setValidateForm1] = useState(false);
+  const [isCheckedQueso1, setIsCheckedQueso1] = useState(false);
+  const [isCheckedQueso2, setIsCheckedQueso2] = useState(false);
+  const [isCheckedQueso3, setIsCheckedQueso3] = useState(false);
+  const [isCheckedQueso4, setIsCheckedQueso4] = useState(false);
+  const [isCheckedQueso5, setIsCheckedQueso5] = useState(false);
+  const [isCheckedQueso6, setIsCheckedQueso6] = useState(false);
+  const [isCheckedQueso7, setIsCheckedQueso7] = useState(false);
+  const [isCheckedQueso8, setIsCheckedQueso8] = useState(false);
+  const [validateForm2, setValidateForm2] = useState(false);
+  const [isCheckedVegetales1, setIsCheckedVegetales1] = useState(false);
+  const [isCheckedVegetales2, setIsCheckedVegetales2] = useState(false);
+  const [isCheckedVegetales3, setIsCheckedVegetales3] = useState(false);
+  const [isCheckedVegetales4, setIsCheckedVegetales4] = useState(false);
+  const [isCheckedVegetales5, setIsCheckedVegetales5] = useState(false);
+  const [isCheckedVegetales6, setIsCheckedVegetales6] = useState(false);
+  const [isCheckedVegetales7, setIsCheckedVegetales7] = useState(false);
+  const [isCheckedVegetales8, setIsCheckedVegetales8] = useState(false);
+  const [validateForm3, setValidateForm3] = useState(false);
 
   function handleCheckboxChange1() {
     setIsChecked1(!isChecked1);
@@ -65,13 +83,129 @@ export const Order = () => {
     
     if (validate.length > 2 ) {
       alert("No puede poner mas de dos ingredientes")
+      setValidateForm1(false)
     }else if( validate.length == 0){
       alert("tiene que poner al menos un ingrediente")
+      setValidateForm1(false)
     }else{
       alert("validado")
+      setValidateForm1(true)
     }
   }
 
+  function handleCheckboxChangeQueso1() {
+    setIsCheckedQueso1(!isCheckedQueso1);
+  }
+
+  function handleCheckboxChangeQueso2() {
+    setIsCheckedQueso2(!isCheckedQueso2);
+  }
+
+  function handleCheckboxChangeQueso3() {
+    setIsCheckedQueso3(!isCheckedQueso3);
+  }
+
+  function handleCheckboxChangeQueso4() {
+    setIsCheckedQueso4(!isCheckedQueso4);
+  }
+
+  function handleCheckboxChangeQueso5() {
+    setIsCheckedQueso5(!isCheckedQueso5);
+  }
+
+  function handleCheckboxChangeQueso6() {
+    setIsCheckedQueso6(!isCheckedQueso6);
+  }
+
+  function handleCheckboxChangeQueso7() {
+    setIsCheckedQueso7(!isCheckedQueso7);
+  }
+
+  function handleCheckboxChangeQueso8() {
+    setIsCheckedQueso8(!isCheckedQueso8);
+  }
+
+  const handleSubmitQueso = (e) =>{
+    e.preventDefault();
+    let arayCheked = [isCheckedQueso1,isCheckedQueso2,isCheckedQueso3,isCheckedQueso4,isCheckedQueso5,isCheckedQueso6,isCheckedQueso7,isCheckedQueso8]
+    let validate = []
+    for (let i = 0; i < 10; i++) {
+      if (arayCheked[i]) {
+        validate.push(arayCheked[1])
+      }
+    }
+    
+    if (validate.length > 2 ) {
+      setValidateForm2(false)
+      alert("No puede poner mas de dos ingredientes")
+      
+    }else if( validate.length == 0){
+      setValidateForm2(false)
+      alert("tiene que poner al menos un ingrediente")
+      
+    }else{
+      alert("validado")
+      setValidateForm2(true)
+    }
+  }
+
+
+  function handleCheckboxChangeVegetales1() {
+    setIsCheckedVegetales1(!isCheckedVegetales1);
+  }
+
+  function handleCheckboxChangeVegetales2() {
+    setIsCheckedVegetales2(!isCheckedVegetales2);
+  }
+
+  function handleCheckboxChangeVegetales3() {
+    setIsCheckedVegetales3(!isCheckedVegetales3);
+  }
+
+  function handleCheckboxChangeVegetales4() {
+    setIsCheckedVegetales4(!isCheckedVegetales4);
+  }
+
+  function handleCheckboxChangeVegetales5() {
+    setIsCheckedVegetales5(!isCheckedVegetales5);
+  }
+
+  function handleCheckboxChangeVegetales6() {
+    setIsCheckedVegetales6(!isCheckedVegetales6);
+  }
+
+  function handleCheckboxChangeVegetales7() {
+    setIsCheckedVegetales7(!isCheckedVegetales7);
+  }
+
+  function handleCheckboxChangeVegetales8() {
+    setIsCheckedVegetales8(!isCheckedVegetales8);
+  }
+
+  const handleSubmitVegetales = (e) =>{
+    e.preventDefault();
+    let arayCheked = [isCheckedVegetales1,isCheckedVegetales2,isCheckedVegetales3,isCheckedVegetales4,isCheckedVegetales5,isCheckedVegetales6,isCheckedVegetales7,isCheckedVegetales8]
+    let validate = []
+    for (let i = 0; i < 10; i++) {
+      if (arayCheked[i]) {
+        validate.push(arayCheked[1])
+      }
+    }
+    
+    if (validate.length > 3 ) {
+      setValidateForm3(false)
+      alert("No puede poner mas de tres ingredientes")
+      
+    }else if( validate.length == 0){
+      setValidateForm3(false)
+      alert("tiene que poner al menos un ingrediente")
+      
+    }else{
+      alert("validado")
+      setValidateForm3(true)
+    }
+  }
+  console.log(validateForm3);
   return (
     <>
       <section className="order-form">
@@ -154,92 +288,110 @@ export const Order = () => {
         <button>Checar</button>
       </form>
 
-      <form>
+      <form onSubmit={handleSubmitQueso}>
         <h3>Queso</h3>{/*2*/}
         <label>Mozarella</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedQueso1}
+          onChange={handleCheckboxChangeQueso1}
         />
         <label>Parmesano</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedQueso2}
+          onChange={handleCheckboxChangeQueso2}
         />
         <label>Cheddar</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedQueso3}
+          onChange={handleCheckboxChangeQueso3}
         />
         <label>Feta</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedQueso4}
+          onChange={handleCheckboxChangeQueso4}
         />
         <label>Cabra</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedQueso5}
+          onChange={handleCheckboxChangeQueso5}
         />
         <label>Ricotta</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedQueso6}
+          onChange={handleCheckboxChangeQueso6}
         />
         <label>Gorgonzola</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedQueso7}
+          onChange={handleCheckboxChangeQueso7}
         />
         <label>Azul</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedQueso8}
+          onChange={handleCheckboxChangeQueso8}
         />
+        <button>Checar</button>
       </form>
       
-      <form>
+      <form onSubmit={handleSubmitVegetales}>
       <h3>Vegetales</h3>{/*3*/}
         <label>Champiñones</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedVegetales1}
+          onChange={handleCheckboxChangeVegetales1}
         />
         <label>Pimiento verde</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedVegetales2}
+          onChange={handleCheckboxChangeVegetales2}
         />
         <label>Maiz</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedVegetales3}
+          onChange={handleCheckboxChangeVegetales3}
         />
         <label>Cebolla</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedVegetales4}
+          onChange={handleCheckboxChangeVegetales4}
         />
         <label>Tomate</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedVegetales5}
+          onChange={handleCheckboxChangeVegetales5}
         />
         <label>Espinacas</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedVegetales6}
+          onChange={handleCheckboxChangeVegetales6}
         />
         <label>Piña</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedVegetales7}
+          onChange={handleCheckboxChangeVegetales7}
         />
         <label>Tomates secos</label>
         <input
           type="checkbox"
-          
+          checked={isCheckedVegetales8}
+          onChange={handleCheckboxChangeVegetales8}
         />
+        <button>Checar</button>
       </form>
       
       <form>
